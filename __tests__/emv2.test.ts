8265340496731183e6c278738e45866764a021c1
Steven Cooper <steven.cooper@securenettech.com>
2020-12-16 17:14:48 -0500

    removed some commented out code to clean up

--- __tests__/emv2.test.ts
+++ __tests__/emv2.test.ts
@@ -34,16 +34,5 @@ describe("employee manager v2", () => {
       let employeeList = await page.getEmployeeList();
       expect(employeeList).not.toContain(newEmployee.name);
     });
-    // await page.addEmployee(newEmployee);
-    // let employee = await page.getCurrentEmployee();
-    // expect(employee.name).toEqual(newEmployee.name);
-    // expect(employee.phone).toEqual(newEmployee.phone);
-    // expect(employee.email).toEqual(newEmployee.email);
-    // expect(employee.title).toEqual(newEmployee.title);
-    // await page.deleteEmployee("Test Employee");
-    // let employeeList = await page.getEmployeeList();
-    // expect(employeeList).not.toContain("Test Employee");
-    // await page.searchFor("Screenshot");
-    // await page.takeScreenshot(`${__dirname}/../screenshot/myscreenshot`);
   });
 });
