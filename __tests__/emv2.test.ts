3c7b8d6f3c0bc67ab6cc688db1f8fd4ddce8c1cb
ShaylaZimmerman <shaylarose16@gmail.com>
2021-10-27 17:05:41 -0500

    Adding these changes to fulfill UI Testing lab requirements!

--- __tests__/emv2.test.ts
+++ __tests__/emv2.test.ts
@@ -1,34 +1,10 @@
+import { findSafariDriver } from "selenium-webdriver/safari";
 import { EmployeeManager, Employee } from "./pageObjects/EmployeeManager";
 
-const employees: Array<Employee> = [
-  {
-    name: "Han Solo",
-    phone: 1111111111,
-    email: "millenium@falcon.rep",
-    title: "Smuggler",
-  },
-  {
-    name: "Luke Skywalker",
-    phone: 2222222222,
-    email: "red5@rogue.rep",
-    title: "Jedi",
-  },
-  {
-    name: "Thrawn",
-    phone: 3333333333,
-    email: "gathrawn@admiralty.emp",
-    title: "Best Villain",
-  },
-  {
-    name: "R2-D2",
-    phone: 4444444444,
-    email: "pottymouth@astromech.rep",
-    title: "Crotchety Old Droid",
-  },
-];
+import * as employees from "./dataAssets/employees.json";
 
 describe("employee manager v2", () => {
-  const page = new EmployeeManager({ browser: "chrome" });
+  const page = new EmployeeManager({ browser: "firefox" });
   beforeEach(async () => {
     await page.navigate();
   });
@@ -41,21 +17,29 @@ describe("employee manager v2", () => {
     let resultList = await page.getEmployeeList();
     expect(originalList.length).toBeGreaterThanOrEqual(resultList.length);
   });
-  test("Can add and delete an employee", async () => {
-    let newEmployee = {
-      name: "Test Employee",
-      phone: 1234567890,
-      email: "test@email.com",
-      title: "test person",
-    };
-    await page.addEmployee(newEmployee);
-    let employee = await page.getCurrentEmployee();
-    expect(employee.name).toEqual(newEmployee.name);
-    expect(employee.phone).toEqual(newEmployee.phone);
-    expect(employee.email).toEqual(newEmployee.email);
-    expect(employee.title).toEqual(newEmployee.title);
-    await page.deleteEmployee("Test Employee");
-    let employeeList = await page.getEmployeeList();
-    expect(employeeList).not.toContain("Test Employee");
+  test("Screenshotting screenshot title employees", async () => {
+  await page.searchFor("Screenshot")
+  await page.takeScreenshot(`${__dirname}/../screenshots/screenshot.png`);
   });
-});
+  test("Screenshotting student title employees", async () => {
+  await page.searchFor("Student")
+  await page.takeScreenshot(`${__dirname}/../screenshots/studentscreenshot.png`);
+  });
+  test("Screenshotting instructor title employees", async () => {
+  await page.searchFor("instructor")
+  await page.takeScreenshot(`${__dirname}/../screenshots/instructorscreenshot.png`);
+  });
+  employees.forEach((newEmployee) => {
+    test(`Can add and delete an employee (newEmployee.name)`, async () => {
+      await page.addEmployee(newEmployee);
+      let employee = await page.getCurrentEmployee();
+      expect(employee.name).toEqual(newEmployee.name);
+      expect(employee.phone).toEqual(newEmployee.phone);
+      expect(employee.email).toEqual(newEmployee.email);
+      expect(employee.title).toEqual(newEmployee.title);
+      await page.deleteEmployee(newEmployee.name);
+      let employeeList = await page.getEmployeeList();
+      expect(employeeList).not.toContain("newEmployee.name");
+    });
+  });
+ });
